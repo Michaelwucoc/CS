0524bfcee62a8156469ca30df2452b07f480b7c2
Michaelwucoc <110759450+Michaelwucoc@users.noreply.github.com>
2024-05-09 19:16:18 +0800

    Add comments in code.

--- Score Counting/While Example 1.py
+++ Score Counting/While Example 1.py
@@ -1,7 +1,13 @@
+# 这是一个计算从1加到n的和的程序，下面是加上中文注释后的代码：
 n = 0
 total = 0
+# 输入要计算的和的上限r
 r = int(input("Enter the value n [1+2+3+...+n]"))
+# 循环变量n从1开始，直到r为止
 while n<r:
+    # n加1
     n=n+1
+    # total加上n
     total = total + n
+# 输出和
 print(total)
